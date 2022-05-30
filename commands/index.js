a8a51d2d9c41d8f21088d30a35868bc3f717ef91
Abhinav Gorantla <abhinav20016@gmail.com>
2022-05-30 19:58:18 +0530

    Added code to check the version of package
    - agcli -i <input> <package_name> now sends a table with the version satisfaction status.

--- commands/index.js
+++ commands/index.js
@@ -3,34 +3,74 @@ import fetch from "node-fetch";
 import fs from "fs";
 
 export default async (filePath, packageName) => {
+    var finalRepos = [];
     const repos = [];
-    console.log(packageName);
     fs.createReadStream(filePath)
         .pipe(csvParser())
         .on('data', (row) => {
             const repo = {
                 name: row.name,
-                url: row.repo
+                url: row.repo,
+                version: '',
+                version_satisfied: null
             }
 
             repos.push(repo);
         })
         .on('end', () => {
-            console.log("Repositories to be tested: ");
-            console.table(repos);
-
             const packageRawUrls = [];
+
+            // get the package.json file URL from github
             for(const i in repos){
                 const tempStr = "https://raw.githubusercontent.com/" + repos[i].url.split(".com")[1] + "/main/package.json";
                 packageRawUrls.push(tempStr);
-                
-                fetch(tempStr).then(res => res.json()).then(text => checkVersion(text, packageName));
+
+                //the required package version details
+                const packageDetails = packageName.split('@');
+
+                // get the package.json file contents from github
+                fetch(tempStr)
+                            .then(res => res.json())
+                            .then(json => {
+                                const deps = json.dependencies;
+                                const currentVersion = deps[packageDetails[0]].split("^")[1];
+                                const requiredVersion = packageDetails[1];
+                                const neaw = {...repos[i], version: currentVersion, version_satisfied: checkVersion(currentVersion, requiredVersion)};
+                                finalRepos.push(neaw);
+                                console.table(finalRepos);
+                            });
+                            // console.table(finalRepos);
+                            
             }
         });
-
-    
+        
 }
 
-function checkVersion(text, packageName){
-    console.log(text.dependencies);
+function checkVersion(packageVersion, requiredVersion) {
+    const packageVersionparts = packageVersion.split('.');
+    const requiredVersionparts = requiredVersion.split('.');
+
+    console.log({requiredVersionparts, packageVersionparts});
+    for (var i = 0; i < packageVersionparts.length; ++i) {
+        if (requiredVersionparts.length == i) {
+            return true;
+        }
+
+        if (packageVersionparts[i] == requiredVersionparts[i]) {
+            continue;
+        }
+        else if (packageVersionparts[i] > requiredVersionparts[i]) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if (packageVersionparts.length != requiredVersionparts.length) {
+        return false;
+    }
+
+    return true;
 }
+
