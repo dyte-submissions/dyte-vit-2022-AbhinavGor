3a42417ab0abbf007245ff0683fe998aadfd4d75
Abhinav Gorantla <abhinav20016@gmail.com>
2022-06-01 14:27:51 +0530

    Added PR feature

--- commands/executeUpdates.js
+++ commands/executeUpdates.js
@@ -10,8 +10,8 @@ const executeUpdates = async (finalRepos, packageName, requiredVersion) => {
         auth: process.env.GITHUB_TOKEN
     });
 
+    const updatedRepos = [];
     finalRepos.map(async (repo) => {
-
         if(!repo.version_satisfied){
             const repoDetails = repo.url.split('/');
         
@@ -25,16 +25,19 @@ const executeUpdates = async (finalRepos, packageName, requiredVersion) => {
             const fileContentBuffer = new Buffer(fileContent, 'base64');
             const fileContentJSON = JSON.parse(fileContentBuffer.toString('ascii'));
             
+            //Checks if the fork has the version of the package satisfied or not, if it is not satisfied,
+            //then we make a commit with the updated version of the package.
             if(!checkVersion(fileContentJSON.dependencies[packageName], requiredVersion)){
                 fileContentJSON.dependencies[packageName] = requiredVersion;
 
                 const updatedFileBuffer = new Buffer(JSON.stringify(fileContentJSON, null, 2));
                 const base64Updates = updatedFileBuffer.toString('base64');
+                const commitMessage = `Chores: Update npm package ${packageName} to ${requiredVersion}.`;
 
                 const commitDetails = {
                     owner: 'AbhinavGor',
                     repo: repoDetails[4],
-                    message: `Chores: Update npm package ${packageName} to ${requiredVersion}.`,
+                    message: commitMessage,
                     committer: {
                         name: process.env.USER_NAME,
                         email: process.env.USER_EMAIL
@@ -43,14 +46,28 @@ const executeUpdates = async (finalRepos, packageName, requiredVersion) => {
                     sha: fileSHA
                 }
 
-                // console.log(commitDetails);
-
                 //make a commit with the changes
-                await octokit.request(`PUT /repos/AbhinavGor/${repoDetails[4]}/contents/package.json`, commitDetails);
+                // await octokit.request(`PUT /repos/AbhinavGor/${repoDetails[4]}/contents/package.json`, commitDetails);
             }
 
-            
+            const prDetails = {
+                owner: 'process.env.OWNER_NAME',
+                repo: repoDetails[4],
+                title: `Chores: Update npm package ${packageName} to ${requiredVersion}.`,
+                head: process.env.USER_NAME + ":main",
+                base: 'main'
+            }
+            //make a PR with the updated changes
+            const pullRes = await octokit.request(`POST /repos/${repoDetails[3]}/${repoDetails[4]}/pulls`, prDetails);
 
+            const newDetails = {...repo, update_pr: pullRes.data.html_url};
+            // console.log({pullRes, newDetails});
+            updatedRepos.push(newDetails);
+
+            console.table(updatedRepos);
+
+        }else{
+            updatedRepos.push({...repo});
         }
     })
     
